fb5f4565f3f9415e03f3d6b8c4dbdc04ecefa66f
Bakhromoff <hotamtoy9007@gmail.com>
2022-06-01 09:59:46 +0500

    add site footer

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const elSpeedyLink = document.querySelector('.js-speedy-search__link');
 const elSpeedyContent = document.querySelector('.js-features-speedy-content');
 const elEasyContent = document.querySelector('.js-features-content-easy');
 const elEasyLink = document.querySelector('.js-easy-features__link');
+
 elSimpleLink.addEventListener('click', function () {
   elSimpleLink.classList.add('features__link--active');
   elsimpleContent.classList.add('features-content--active');
@@ -27,4 +28,25 @@ elEasyLink.addEventListener('click', function () {
   elSpeedyContent.classList.remove('features-content--active');
   elSimpleLink.classList.remove('features__link--active');
   elsimpleContent.classList.remove('features-content--active');
+});
+
+
+
+const elsFaqButton = document.querySelectorAll('.js-faq-button');
+const elsFaqItem = document.querySelectorAll('.faq__item');
+
+
+// function close(){
+//   elsFaqItem.forEach(function(item){
+//     item.classList.remove('faq__item--active')
+//   })
+// }
+
+elsFaqButton.forEach(function (elFaqButton) {
+  elFaqButton.addEventListener('click', function () {
+    elsFaqItem.forEach(function(item){
+      item.classList.remove('faq__item--active')
+    })
+    elFaqButton.closest('.faq__item').classList.add('faq__item--active');
+  });
 });
